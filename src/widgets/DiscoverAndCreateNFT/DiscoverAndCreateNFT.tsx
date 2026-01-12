1642db4ea323150b435e42511512914811436597
dklimov <auer.klimov@gmail.com>
2026-01-12 03:39:39 +0300

    Свёрстан раздел DiscoverAndCreateNFT

--- src/widgets/DiscoverAndCreateNFT/DiscoverAndCreateNFT.tsx
+++ src/widgets/DiscoverAndCreateNFT/DiscoverAndCreateNFT.tsx
@@ -6,12 +6,45 @@ const DiscoverAndCreateNFT = () => {
   return (
     <div className={styles.root}>
       <div className={styles.container}>
+        <div className={styles.infoSection}>
+          <div className={styles.mainCaption}>
+            <Font family='Poppins' weight='SemiBold'>
+              OVER 1M CREATERS
+            </Font>
+          </div>
+          <h2>
+            <Font family='Poppins' weight='SemiBold'>
+              Discover And Create NFTs
+            </Font>
+          </h2>
+          <h3>
+            <Font family='Inter' weight='Regular'>
+              Discover, Create and Sell NFTs On Our NFT Marketplace With Over Thousands Of NFTs And Get a <b>$20 bonus</b>.
+            </Font>
+          </h3>
+          <div className={styles.buttons}>
+            <button>EXPLORE MORE</button>
+            <button>CREATE NFT</button>
+          </div>
+          <div className={styles.stats}>
+            <div>
+              <div className={styles.value}><Font family='PublicSans' weight='SemiBold'>430K+</Font></div>
+              <div className={styles.caption}><Font family='Inter' weight='Medium'>Art Works</Font></div>
+            </div>
+            <div>
+              <div className={styles.value}><Font family='PublicSans' weight='SemiBold'>159K+</Font></div>
+              <div className={styles.caption}><Font family='Inter' weight='Medium'>Creators</Font></div>
+            </div>
+            <div>
+              <div className={styles.value}><Font family='PublicSans' weight='SemiBold'>87K+</Font></div>
+              <div className={styles.caption}><Font family='Inter' weight='Medium'>Collections</Font></div>
+            </div>
+          </div>
+        </div>
 
-
-        <h2>Discover And Create NFTs</h2>
-        <Spiral />
-        <div>
-          <Font family='Outfit' weight='SemiBold'>Explore More</Font>
+        <div className={styles.grapchicSection}>
+          <img src="/static/images/im1.jpg" width="350" />
+          <Spiral />
         </div>
       </div>
     </div>
